94f651afad7e29581d43fe73402220bb4ef73660
Alwyn Kik <alwyn@kik.pw>
2022-02-10 22:27:18 +0100

    Print the maze after loading

--- pkg/maze/run.go
+++ pkg/maze/run.go
@@ -2,11 +2,39 @@ package maze
 
 import (
 	"fmt"
+	"log"
+	"strings"
 	"time"
+
+	"almaze/pkg/models"
 )
 
-func Run() {
-	fmt.Printf("The current time is: %v\n", time.Now())
+var wallSymbol = "@"
 
-	LoadMaze("maze.txt")
+func BuildMaze(maze models.Maze) string {
+	var sb strings.Builder
+
+	for _, mf := range maze.Fields {
+		if mf.Wall {
+			sb.WriteString(wallSymbol)
+		} else {
+			sb.WriteString(" ")
+		}
+
+		// End of line reached
+		if mf.X == maze.Width {
+			sb.WriteString("\n")
+		}
+	}
+
+	return sb.String()
+}
+
+func Run() {
+	log.Printf("The current time is: %v\n", time.Now())
+
+	//LoadMaze("maze.txt")
+	myMaze := LoadMaze("maze.txt")
+	mazeString := BuildMaze(myMaze)
+	fmt.Print(mazeString)
 }
